a94ebc38aece96a4a22b18fd2bcbb5534a2eb209
heylarissa <larissah.dandrade@gmail.com>
2023-10-27 16:13:56 -0300

    treinando modelo acurácia 43

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Implementation of the TF-IDF algorithm to identify the most important words in e
 Google has been using TF-IDF (or TF  IDF, TF*IDF, TFIDF, TF.IDF) to rank your content for a long time. It seems that Google focuses more on term frequency rather than on counting keywords. 
 
 So, it’s important to wrap your head around the TF-IDF algorithm and how it works.
+
+https://www.geeksforgeeks.org/understanding-tf-idf-term-frequency-inverse-document-frequency/
+
+Passo 1: Coletar e preparar os dados  
+O primeiro passo é coletar o conjunto de documentos que compõem o seu corpus, que podem ser páginas de um site, artigos, posts de blog, entre outros. Em seguida, deve-se preparar os textos, removendo qualquer informação irrelevante, como pontuação e caracteres especiais, além de aplicar técnicas de pré-processamento de linguagem natural, como a lematização e a remoção de stopwords (palavras comuns que não agregam valor semântico).  
+
+Passo 2: Calcular o Term Frequency (TF)  
+Para calcular o TF para cada termo em um documento, conte quantas vezes cada um aparece no texto e divida esse valor pelo total de palavras no documento.
+
+Passo 3: Calcular o Inverse Document Frequency (IDF)  
+Utilize a fórmula mencionada anteriormente para calcular o IDF para cada termo.  
+
+Passo 4: Calcular o TF-IDF  
+Agora que temos os valores do TF e IDF para cada termo e documento, podemos calcular o TF-IDF para cada termo em cada documento, multiplicando os valores do TF pelo IDF.
+
+Passo 5: Interpretar os resultados do cálculo  
+Após calcular o TF-IDF para todos os termos em todos os documentos, é hora de interpretar os resultados. Identifique os termos com maiores valores de TF-IDF em cada documento, pois eles são os mais relevantes para aquele conteúdo específico.
--- main.py
+++ main.py
@@ -10,28 +10,81 @@ Etapas para classificação do texto utilizando o algoritmo TF-IDF
 
 """
 
-dados = pd.read_csv('g1_v1.csv')
-file = open('words.txt', 'w+')
+def term_frequency():
+    pass
+
+dados = pd.read_csv("g1_v1.csv")
+file = open("words.txt", "w+")
+vocab = []  # palavras distintas
 document = []
-stop_words = set(stopwords.words('portuguese'))
+frequency = []  # frequência de recorrência
+stop_words = set(stopwords.words("portuguese"))  # palavras não essenciais
+data = []
 
-for text in dados['texto']:
+# merge documents into a single corpus
+for text in dados["texto"]:
     # tokenizar (separa em tokens)
-    words = text.lower().split(' ')
+    w = text.lower()
     
+    # remove números e pontuação
+    words = w.replace("'", "")
+    for c in '123456789.,:?/#"':
+        words = words.replace(c, "") 
+
+    data.append(words)
+    
+    words = words.split(" ")
     for word in words:
+        if word not in stop_words:
+            document.append(word)
+            if word not in vocab:
+                # remove palavras repetidas e stop words
+                file.write(word + ", ")
+                vocab.append(word)
 
-        # remove números e pontuação
-        w = word.replace("'", '')
+for word in vocab:
+    frequency.append(document.count(word))
 
-        for c in '123456789.,:?/#"':
-            w = w.replace(c, '')
+words_frequency = []
+i = 0
+for word in vocab:
+    words_frequency.append({'word': word, 'frequency': frequency[i]}) 
+    i+=1
 
-        if w not in document and w not in stop_words:
-            file.write(w + ', ')
-            document.append(w)
-
-print('Tamanho do vocabulário: ' + str(len(document)))
-
+print("Tamanho do vocabulário: " + str(len(vocab)))
+# print(words_frequency)
 file.close()
+
+# import required module
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+# create object
+tfidf = TfidfVectorizer()
+ 
+# get tf-df values
+result = tfidf.fit_transform(data)
+print(result)
+X_train, X_test, y_train, y_test = train_test_split(result, dados['classe'], test_size=0.3, random_state=42)
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+# Divisão dos dados e treinamento do modelo
+
+rf_model = RandomForestClassifier(n_estimators=300, random_state=42) 
+# n_estimators = number of trees in the forest
+
+rf_model.fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+confusion = confusion_matrix(y_test, y_pred)
+print("Matriz de Confusão:")
+print(confusion)
+print()
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Acurácia: ", accuracy)
+print()
+print(classification_report(y_test, y_pred))
 
